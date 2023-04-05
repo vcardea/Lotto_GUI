f3f742be73ed5c1570f57c68f6455cebb31d36a2
cestello <107208390+cestello@users.noreply.github.com>
2023-04-05 20:16:16 +0200

    .

--- Finestre/FinestraLogin.java
+++ Finestre/FinestraLogin.java
@@ -1,5 +1,28 @@
 package Finestre;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
 public class FinestraLogin {
-    
+    public static void main(String args[]) {
+        JFrame jf = new JFrame("Login");
+        JPanel jp = new JPanel();
+        JLabel jl1 = new JLabel("Gioco del lotto");
+        JLabel jl2 = new JLabel("username", JLabel.LEFT);
+        // JButton jb = new JButton("OK");
+        // JTextField jtf = new JTextField(15);
+
+        jp.add(jl1);
+        jp.add(jl2);
+        jf.add(jp);
+
+        GestoreFinestra g = new GestoreFinestra();
+        jf.addWindowListener(g);
+
+        jf.setSize(350, 100);
+        jf.setVisible(true);
+    }
 }
---
+++ Finestre/start.java
@@ -0,0 +1,5 @@
+package Finestre;
+
+public class start {
+
+}
