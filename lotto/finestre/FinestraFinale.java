f582cdb2a875a336bdb39c904f7dbbf3d4cc5427
vcardeaa <vincenzo.cardea.05@gmail.com>
2023-04-19 11:26:26 +0200

    Aggiornamento 19.04

--- lotto/finestre/FinestraFinale.java
+++ lotto/finestre/FinestraFinale.java
@@ -5,11 +5,23 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 
-import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class FinestraFinale {
+
+    public class GestoreInterno implements ActionListener {
+        public void actionPerformed(ActionEvent ae) {
+            String s = ae.getActionCommand();
+            if (s.equals("chiudi")) {
+                jf.dispose();
+            }
+        }
+    }
+
+    private final byte PANELS = 5;
     private JFrame jf = new JFrame();
-    private JPanel jp = new JPanel();
+    private JPanel[] jp = new JPanel[PANELS];
     private JLabel jlTitolo = new JLabel("Risultato Finale");
     private JLabel jlNumeriEstratti = new JLabel("Numeri estratti");
     private JLabel jlEstratti = new JLabel();
@@ -20,8 +32,30 @@ public class FinestraFinale {
     private JButton jbChiudi = new JButton("Chiudi");
 
     public FinestraFinale() {
-        jp.setLayout(new GridLayout(5, 1));
+        for (int i = 0; i < jp.length; i++) {
+            jp[i] = new JPanel();
+        }
 
+        // Aggiunge componenti al pannello
+        jp[0].add(jlTitolo);
+        jp[1].add(jlNumeriEstratti);
+        jp[1].add(jlEstratti);
+        jp[2].add(jlNumeriIndovinati);
+        jp[2].add(jlIndovinati);
+        jp[3].add(jlTitoloGuadagno);
+        jp[3].add(jlGuadagno);
+        jp[4].add(jbChiudi);
+
+        // Pulsante di chiusura
+        jbChiudi.setActionCommand("chiudi");
+        jbChiudi.addActionListener(new GestoreInterno());
+
+        // 
+        for (int i = 0; i < jp.length; i++) {
+            jf.add(jp[i]);
+        }
+
+        // jf.addWindowListener(new GestoreFinestra());
         jf.setSize(400, 400);
         jf.setVisible(true);
     }
