4a05d5d2e5cac7ddbeb1e24d1b6c8d8681806a63
cestello <107208390+cestello@users.noreply.github.com>
2023-04-22 20:42:36 +0200

    Create compile.txt

---
+++ compile.bat
@@ -0,0 +1,16 @@
+javac lotto/finestre/gestori/GestoreFinestra.java
+javac lotto/finestre/gestori/GestoreUscita.java
+
+javac lotto/finestre/FinestraConfigurazioneLotto.java
+javac lotto/finestre/FinestraFinale.java
+javac lotto/finestre/FinestraInserimentoNumeri.java
+javac lotto/finestre/FinestraLogin.java
+javac lotto/finestre/FinestraMenu.java
+
+javac lotto/calcoli/CalcoloVincita.java
+
+javac lotto/utente/Utente.java
+
+javac lotto/main/LottoMain.java
+
+java lotto/main/LottoMain
