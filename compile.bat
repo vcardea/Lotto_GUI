be692743075e20e11292a166f6d84a92c303bf33
vcardeaa <vincenzo.cardea.05@gmail.com>
2023-04-26 10:11:54 +0200

    Aggiornamento file .bat

--- compile.bat
+++ compile.bat
@@ -1,12 +1,14 @@
 :: Diverse ccompilazioni
 javac src/finestre/gestori/GestoreUscita.java
 javac src/finestre/gestori/GestoreFinestra.java
+javac src/finestre/gestori/GestoreFinestraND.java
+javac src/finestre/gestori/GestoreFinestraFN.java
 
-javac src/finestre/FinestraConfigurazioneLotto.java
-javac src/finestre/FinestraFinale.java
-javac src/finestre/FinestraInserimentoNumeri.java
-javac src/finestre/FinestraLogin.java
-javac src/finestre/FinestraMenu.java
+javac src/finestre/ConfigurazioneLotto.java
+javac src/finestre/Finale.java
+javac src/finestre/InserimentoNumeri.java
+javac src/finestre/Login.java
+javac src/finestre/Menu.java
 
 javac src/calcoli/CalcoloVincita.java
 
