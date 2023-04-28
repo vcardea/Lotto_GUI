85d201b556cebe50bd9947254e9440b75e21b918
francesco <107208390+cestello@users.noreply.github.com>
2023-04-28 20:32:40 +0200

    Output

--- src/finestre/Finale.java
+++ src/finestre/Finale.java
@@ -58,6 +58,8 @@ public class Finale {
     private float vincita = 0.0f;
 
     private final byte PANELS = 3;
+    private String numeriEstrattiString = new String("");
+    private String numeriIndovinatiString = new String("");
     private JFrame jf = new JFrame(UtilitiesFinestra.TITOLO);
     private JPanel[] jp = new JPanel[PANELS];
     private GridLayout gl = new GridLayout(6, 1);
@@ -73,9 +75,11 @@ public class Finale {
     public Finale(boolean[] numeriScelti, float importo, byte numeri) {
         estrai();
         for (byte i = 0; i < 90; ++i) {
+            if(checkEstrazioni[i]) numeriEstrattiString += i + 1 + " ";
             if (numeriScelti[i] && checkEstrazioni[i]) {
                 numeriIndovinati.addElement((byte) (i + 1));
                 ++contNumeriVinti;
+                numeriIndovinatiString += i + 1 + " ";
             }
         }
         CalcoloVincita cv = new CalcoloVincita(numeri, importo, contNumeriVinti);
@@ -98,15 +102,16 @@ public class Finale {
 
         // Imposta il layout del pannello centrale
         jp[1].setLayout(gl);
-
+        System.out.println(numeriIndovinatiString.length());
         // Aggiunge componenti al pannello
         jp[0].add(jlTitolo);
         jp[1].add(jlNumeriEstratti);
-        jp[1].add(new JLabel("1 2 3\n4 5 6\n7 8 9", JLabel.CENTER));
+        jp[1].add(new JLabel(numeriEstrattiString, JLabel.CENTER));
         jp[1].add(jlNumeriIndovinati);
-        jp[1].add(new JLabel("Secondo"));
+        if(numeriEstrattiString.length() != 0) jp[1].add(new JLabel(numeriIndovinatiString, JLabel.CENTER));
+        else jp[1].add(new JLabel("Non hai indovinato numeri", JLabel.CENTER));
         jp[1].add(jlTitoloGuadagno);
-        jp[1].add(new JLabel("Terzo"));
+        jp[1].add(new JLabel(String.valueOf(vincita), JLabel.CENTER));
         jp[2].add(jbChiudi, BorderLayout.CENTER);
 
         // Pulsante di chiusura
