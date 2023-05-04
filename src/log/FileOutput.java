ca2859cb6a276c477d3942c15cc2bffcce5c6686
cestello <107208390+cestello@users.noreply.github.com>
2023-05-04 19:49:55 +0200

    Documentazione FileOutput

--- src/log/FileOutput.java
+++ src/log/FileOutput.java
@@ -4,15 +4,27 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
 
+/**
+ * Classe con funzioni utili per Output da file
+ */
 public class FileOutput {
     private final String OUTPUT;
     private FileWriter fw;
     private PrintWriter pw;
     
+    /**
+     * Costruttore
+     * @param OUTPUT percorso del file di Output
+     */
     public FileOutput(String OUTPUT) {
         this.OUTPUT = OUTPUT;
     }
 
+    /**
+     * Apre il flusso di Output
+     * @param append per capire se il file va creato o solo modificato
+     * @return se ci sono stati problemi nell'apertura del flusso
+     */
     private boolean openOutput(boolean append) {
         try {
             fw = new FileWriter(OUTPUT, append);
@@ -24,6 +36,9 @@ public class FileOutput {
         return true;
     }
 
+    /**
+     * Chiude il flusso di Output
+     */
     private void closeOutput() {
         try {
             fw.close();
@@ -32,6 +47,10 @@ public class FileOutput {
         }
     }
 
+    /**
+     * @param LINE linea letta da inserire nel file
+     * @param append per capire se il file va riscritto o solo modificato
+     */
     public void write(String LINE, boolean append) {
         if (openOutput(append)) {
             pw.println(LINE);
